563d14eada10d65882de3c9e7c73d3181274f63f
Timothy Nibert <docz2a@gmail.com>
2026-01-19 22:11:40 +1100

    sieve of eratosthenes as a python generator

---
+++ sieve.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+
+import time
+
+"""
+Generator to generate the series of prime numbers.
+Implementation of the "sieve of Eratosthenes" from SICP 3.5.2 as a python generator.
+Apparently not the real sieve of Eratosthenes, apparently actually Turner's sieve.
+"""
+
+WANT_TO_GO_HIGHER=False
+
+def divisible(x, y):
+    return x % y == 0
+
+def integers_starting_from(n):
+    while True:
+        yield n
+        n += 1
+
+def sieve(stream):
+    car = next(stream)
+    yield car
+    for n in sieve(filter(lambda x: not divisible(x, car), stream)):
+        yield n
+
+def primes():
+    for prime in sieve(integers_starting_from(2)):
+        yield prime
+
+if __name__ == '__main__':
+    if WANT_TO_GO_HIGHER:
+        import sys
+        print("Recursion limit:\t{}".format(sys.getrecursionlimit()))
+        sys.setrecursionlimit(20000)
+        print("New limit:\t{}".format(sys.getrecursionlimit()))
+
+    for p in primes():
+        print(p)
+        time.sleep(1)
